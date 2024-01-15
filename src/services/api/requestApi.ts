dd98af22d6bd774727ca9eb4c6959e9ffe9937bf
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-01-15 11:42:00 +0300

    fix: убран '/' в vite.config.ts в url. В requestApi добавлены '/'

--- src/services/api/requestApi.ts
+++ src/services/api/requestApi.ts
@@ -6,26 +6,26 @@ type TPeriod = 'day' | 'week';
 
 export const REQUEST_URLS = {
     genres: {
-        movie: '3/genre/movie/list?language=en',
-        tvSeries: '3/genre/tv/list?language=en'
+        movie: '/3/genre/movie/list?language=en',
+        tvSeries: '/3/genre/tv/list?language=en'
     },
     trendings: {
-        all: (period: TPeriod, page: number) => `3/trending/all/${period}?language=en-US&page=${page}`,
-        movie: (period: TPeriod, page: number) => `3/trending/movie/${period}?language=en-US&page=${page}`,
-        tv: (period: TPeriod, page: number) => `3/trending/tv/${period}?language=en-US&page=${page}`
+        all: (period: TPeriod, page: number) => `/3/trending/all/${period}?language=en-US&page=${page}`,
+        movie: (period: TPeriod, page: number) => `/3/trending/movie/${period}?language=en-US&page=${page}`,
+        tv: (period: TPeriod, page: number) => `/3/trending/tv/${period}?language=en-US&page=${page}`
     },
     searchBy: {
       genres: (arrayOfIds: number[], mainType: TGenresFor, page: number) => getSearchedByIdURL(arrayOfIds, mainType, page),
-      keyWord: (query: string, page: number, mainType: TGenresFor) => `3/search/${mainType === 'tvSeries' ? 'tv' : 'movie'}?query=${query}&include_adult=false&language=en-US&page=${page}`,
-      id: (id: number, mainType: TGenresFor) => `3/${mainType === 'tvSeries' ? 'tv' : 'movie'}/${id}?language=en-US`,
-      personId: (id: number) => `3/person/${id}`,
-      similar: (id: number) => `3/movie/${id}/similar`
+      keyWord: (query: string, page: number, mainType: TGenresFor) => `/3/search/${mainType === 'tvSeries' ? 'tv' : 'movie'}?query=${query}&include_adult=false&language=en-US&page=${page}`,
+      id: (id: number, mainType: TGenresFor) => `/3/${mainType === 'tvSeries' ? 'tv' : 'movie'}/${id}?language=en-US`,
+      personId: (id: number) => `/3/person/${id}`,
+      similar: (id: number) => `/3/movie/${id}/similar`
     },
     getCredits: {
-      movie: (id: number) => `3/movie/${id}/credits?language=en-US`,
-      tv: (id: number) => `3/tv/${id}/credits?language=en-US`,
-      aggregateTV: (id: number) => `3/tv/${id}/aggregate_credits`,
-      combinedCredits: (id: number) => `3/person/${id}/combined_credits`
+      movie: (id: number) => `/3/movie/${id}/credits?language=en-US`,
+      tv: (id: number) => `/3/tv/${id}/credits?language=en-US`,
+      aggregateTV: (id: number) => `/3/tv/${id}/aggregate_credits`,
+      combinedCredits: (id: number) => `/3/person/${id}/combined_credits`
     },
     images: {
       IMG_200: "https://image.tmdb.org/t/p/w200",
