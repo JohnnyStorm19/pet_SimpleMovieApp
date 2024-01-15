e007da20e9b8cf17428378407f07f21de1a6eeda
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-01-15 13:57:26 +0300

    added new basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={'movie-pet-app.vercel.app'}>
+    <BrowserRouter basename={'movie-pet-app1.vercel.app'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
