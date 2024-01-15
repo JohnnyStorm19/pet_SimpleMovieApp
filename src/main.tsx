6ac9de5d33ca805265ff8e347ef2c23ab671eaf4
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-01-15 13:52:34 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={'movie-pet-5kvoq3ley-johnnystorm19s-projects.vercel.app/'}>
+    <BrowserRouter basename={'movie-pet-app.vercel.app'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
