5b4a9b3568385d95efada6cf7e786e9a8b6df593
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-01-15 14:35:39 +0300

    fix: .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || 'https://pet-simple-movie-app.vercel.app/'
+  base: 'https://pet-simple-movie-app.vercel.app/'
 })
+
+console.log(process.env.VITE_BASE_PATH);
